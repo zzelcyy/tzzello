5286df91dcdb5f8ddbdf408de141c7ee9c9755dd
CHELSEA <39520703+Chaeeun-Lee@users.noreply.github.com>
2021-12-11 00:10:31 +0900

    etc: browser router 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
-const App: FC = () => <BrowserRouter></BrowserRouter>;
+const App: FC = () => <div />;
 
 export default App;
