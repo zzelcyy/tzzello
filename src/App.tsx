6556ab79c13f622ce9314a1a63f53663ac0e5a5a
CHELSEA <39520703+Chaeeun-Lee@users.noreply.github.com>
2021-12-11 00:48:05 +0900

    uiux: global style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import { Board } from 'components/index';
+import GlobalStyle from 'lib/styles/GlobalStyle';
 
 const App: FC = () => (
   <div>
+    <GlobalStyle />
     <Board />
   </div>
 );
---
+++ src/lib/styles/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body, p {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: gray;
+  }
+`;
+
+export default GlobalStyle;
