b2daba880bec6b6bf2ae64d63c01ebe926e8815a
CHELSEA <39520703+Chaeeun-Lee@users.noreply.github.com>
2021-12-11 00:35:53 +0900

    uiux: 새 작업 추가 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
+import { Button } from 'lib/styles';
 
-const App: FC = () => <div />;
+const App: FC = () => (
+  <div>
+    <Button>새 작업 추가</Button>
+  </div>
+);
 
 export default App;
